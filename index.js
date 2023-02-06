39dbb705434812c2a89d708ace7c3d0b9e7ed513
nhihv <nhi.hv@vn-cubesystem.com>
2023-02-07 00:21:16 +0700

    upda te 6

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Test update 4" }');
+    res.send('{ "response": "Test update 5" }');
 });
 
 app.get('/will', function (req, res) {
