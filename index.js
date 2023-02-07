4c409e70fa1baa92cae40a8d072fe361ebb52f41
nhihv <nhi.hv@vn-cubesystem.com>
2023-02-08 01:55:49 +0700

    test 7

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Test update 6" }');
+    res.send('{ "response": "Test update 7" }');
 });
 
 app.get('/will', function (req, res) {
