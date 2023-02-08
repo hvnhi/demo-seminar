af93b244efbfca04819160e9436459b07b452895
nhihv <nhi.hv@vn-cubesystem.com>
2023-02-08 08:39:04 +0700

    test auto deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Test update 7" }');
+    res.send('{ "response": "Test update 8" }');
 });
 
 app.get('/will', function (req, res) {
