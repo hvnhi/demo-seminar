396f9ce76d364b3aae31c7086bf840d05ff8cd3d
nhihv <nhi.hv@vn-cubesystem.com>
2023-02-15 13:53:22 +0700

    update seminar

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
     res.send(`
     <div class="scene">
         <div class="cube-wrapper">
-            <a href="https://www.rareprogrammer.com/bouncing-cube-css-loader">
+            <a href="#">
             <div class="cube">
             <div class="cube-faces">
                 <div class="cube-face shadow"></div>
@@ -21,7 +21,7 @@ app.get('/', function (req, res) {
             </a>
         </div>
     </div>
-    <div style="font-size: 36px; font-weight: bold">Welcome to seminar!!!</div>
+    <div style="font-size: 36px; font-weight: bold">Welcome to Jenkins seminar!!!</div>
     <ol style="font-size: 24px">
         <li>Giới thiệu mô hình</li>
         <li>Jenkins và Syntax</li>
