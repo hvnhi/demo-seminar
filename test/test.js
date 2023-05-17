7ef0e4ec8fd4106e7d210868c36ca17aad96310e
CSV Nhi <nhi.hv@vn-cubesystem.com>
2023-05-17 23:28:19 +0700

    test pull request

--- test/test.js
+++ test/test.js
@@ -2,6 +2,6 @@ var request = require('supertest');
 var app = require('../index.js');
 describe('GET /will', function() {
     it('respond with hello world', function(done) {
-        request(app).get('/will').expect('{ "response": "Hello World 123!!!!!" }', done);
+        request(app).get('/will').expect('{ "response": "Hello World 12345!!!!!" }', done);
     });
 });
